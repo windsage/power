e3397a87d4e66377132073893764eba6ea235481
chao.xu5 <chao.xu5@transsion.com>
2025-08-08 17:07:45 +0800

    delete useless

--- Android.mk
+++
@@ -1,7 +0,0 @@
-#Split build wrap
-ifdef MTK_TARGET_PROJECT
-
-LOCAL_PATH:= $(call my-dir)
-include $(call all-makefiles-under, $(LOCAL_PATH))
-
-endif #MTK_TARGET_PROJECT
--- lib/Android.mk
+++
@@ -1,3 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-
-include $(call all-named-subdir-makefiles,powerhal powerhalwrap_v_2_0 mtkperf_client)
